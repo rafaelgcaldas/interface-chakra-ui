ca5268c1573889702194dc27242d31e9be80f19e
Rafael <rafael.gcaldas01@gmail.com>
2022-01-24 23:05:04 -0300

    Config colors

--- src/styles/theme.ts
+++ src/styles/theme.ts
@@ -1,11 +1,26 @@
 import { extendTheme } from '@chakra-ui/react'
 
 export const theme = extendTheme({
+  colors: {
+    gray: {
+      "400": "#47585B",
+      "300": "#999999",
+      "200": "#DADADA",
+      "100": "rgba(153, 153, 153, 0.5)",
+    },
+    yellow: {
+      "900": "#FFBA08",
+      "400": "#rgba(255, 186, 8, 0.5)",
+    },
+    blue: {
+      "100": "#F5F8FA"
+    }
+  },
   styles: {
     global: {
       body: {
         bg: '#DADADA',
-        color: 'gray.50'
+        color: 'gray.400'
       }
     }
   }
