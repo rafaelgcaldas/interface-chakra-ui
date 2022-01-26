2dd58a7b3c22deabd5e9b2b26bc68ab97a69d17e
Rafael <rafael.gcaldas01@gmail.com>
2022-01-25 23:24:05 -0300

    Banner component

---
+++ src/components/Banner/index.tsx
@@ -0,0 +1,43 @@
+import { Box, Flex, Heading, VStack, Image, Text, Grid, GridItem } from '@chakra-ui/react'
+
+export function Banner() {
+  return (
+    <Box
+        backgroundImage="url('/images/background.png')"
+        backgroundPosition="center"
+        backgroundRepeat="no-repeat"
+        height="335px"
+        width="100%"
+      >
+        <Box
+          maxWidth="1240px"
+          mx="auto"
+        >
+          <Grid templateColumns='repeat(5, 1fr)'>
+            <GridItem colSpan={3}>
+              <VStack
+                padding="5rem"
+                color="blue.100"
+                align="flex-start"
+              >
+                <Box mb="4">
+                  <Heading>5 Continents</Heading>
+                  <Heading>infinitas possibilidades.</Heading>
+                </Box>
+                
+                <Text fontSize="lg" >
+                  Chegou a hora de tirar do papel a viagem que você sempre sonhou. 
+                </Text>
+              </VStack>
+            </GridItem>
+
+            <GridItem colSpan={2}>
+              <Image mt="24" width="417px" src='/images/airplane.png' alt='airplane' />
+            </GridItem>
+          </Grid>
+         
+        </Box>
+
+      </Box>
+  )
+}
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,3 +1,4 @@
+import { Banner } from '../components/Banner'
 import { Header } from '../components/Header'
 
 
@@ -5,6 +6,7 @@ export default function Home() {
   return (
     <>
       <Header />
+      <Banner />
     </>
   )
 }
