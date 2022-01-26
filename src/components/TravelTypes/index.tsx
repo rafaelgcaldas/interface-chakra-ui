f49c77ceafa9ec0c6edc9476f6f9f1ad24f3651d
Rafael <rafael.gcaldas01@gmail.com>
2022-01-25 23:44:12 -0300

    TravelTypes component

---
+++ src/components/TravelTypes/index.tsx
@@ -0,0 +1,16 @@
+import { SimpleGrid, Box } from '@chakra-ui/react'
+import { TravelItem } from '../TravelItem'
+
+export function TravelTypes() {
+  return (
+    <Box maxWidth="1160px" mx="auto" my="5rem">
+      <SimpleGrid minChildWidth='120px' spacing='40px'>
+        <TravelItem label="vida noturna" img="/images/cocktail.svg" />
+        <TravelItem label="praia" img="/images/surf.svg" />
+        <TravelItem label="moderno" img="/images/building.svg" />
+        <TravelItem label="clássico" img="/images/museum.svg" />
+        <TravelItem label="e mais..." img="/images/earth.svg" />
+      </SimpleGrid>
+    </Box>
+  )
+}
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,6 @@
-import { SimpleGrid, Box, VStack, Image, Text } from '@chakra-ui/react'
 import { Banner } from '../components/Banner'
 import { Header } from '../components/Header'
-import { TravelItem } from '../components/TravelItem'
+import { TravelTypes } from '../components/TravelTypes'
 
 
 export default function Home() {
@@ -9,17 +8,7 @@ export default function Home() {
     <>
       <Header />
       <Banner />
-
-      <Box maxWidth="1160px" mx="auto" my="5rem">
-        <SimpleGrid minChildWidth='120px' spacing='40px'>
-          <TravelItem label="vida noturna" img="/images/cocktail.svg" />
-          <TravelItem label="praia" img="/images/surf.svg" />
-          <TravelItem label="moderno" img="/images/building.svg" />
-          <TravelItem label="clássico" img="/images/museum.svg" />
-          <TravelItem label="e mais..." img="/images/earth.svg" />
-        </SimpleGrid>
-      </Box>
-
+      <TravelTypes />
     </>
   )
 }
